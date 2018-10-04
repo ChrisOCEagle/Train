d2c6527b6399e2c04797295779af73943144ca09
Siberianeagle5 <christopherdavyydoconnell.com>
2018-10-04 18:46:47 -0400

    Almost done now for application of moment

--- assets/javascript/Moment.js
+++ assets/javascript/Moment.js
@@ -22,8 +22,9 @@
     var trainTime = $("#train-time").val().trim();
     var frequency = parseInt($("#frequency").val().trim());
 
+    // create a temporary object to store
     // save the input values to the database
-    database.ref().set({
+    database.ref('/trainSchedule').push({
       name: trainName,
       destination: destination,
       "Starting Time": trainTime,
@@ -39,21 +40,23 @@
 
   // create a firebase event for adding a train schedule to the database
   // also add a row in the html once the user adds to the database
-  database.ref().on("child_added", function(snapshot) {
-    // store all data in a variable
+  database.ref('/trainSchedule').on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+    // create a variable to represent each thing in the database
     var trainName = snapshot.val().name,
         destination = snapshot.val().destination,
         trainTime = snapshot.val()["Starting Time"],
         frequency = snapshot.val().frequency;
-    
-    // create a new row and append the data to it
+    console.log(trainName);
+    console.log(destination);
+    console.log(trainTime);
+    console.log(frequency);
+    // add a new row and append the data to the table
     var newRow = $("<tr>").append(
       $("<td>").text(trainName),
       $("<td>").text(destination),
-      $("<td>").text(trainTime),
       $("<td>").text(frequency)
     );
-
     // append the new row to the table
     $("#train-schedule > tbody").append(newRow);
   });

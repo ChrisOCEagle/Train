99790015ae7821308f5f2cecff75dfd23d36b396
Siberianeagle5 <christopherdavyydoconnell.com>
2018-10-06 00:13:53 -0400

    finished with the functionality with the exception of my moment calculation is making all of my differences appear in fractional seconds and I have no clue why. Now just to work on styling it which I can do after submission!

--- assets/javascript/Moment.js
+++ assets/javascript/Moment.js
@@ -1,3 +1,5 @@
+// initialize the document
+$(document).ready(function() {
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyAsJk_huQ39c3TTbasv7qmyX953VUCg95U",
@@ -11,6 +13,9 @@
 
   // creating a variable to access firebase from
   var database = firebase.database();
+
+  // creates a variable for the current time
+  var currentTime = moment().format("HH:mm A");
 
   $("#submit").on("click", function(event){
     // prevent the form from submitting
@@ -41,22 +46,33 @@
   // create a firebase event for adding a train schedule to the database
   // also add a row in the html once the user adds to the database
   database.ref('/trainSchedule').on("child_added", function(snapshot) {
-    console.log(snapshot.val());
     // create a variable to represent each thing in the database
     var trainName = snapshot.val().name,
         destination = snapshot.val().destination,
         trainTime = snapshot.val()["Starting Time"],
         frequency = snapshot.val().frequency;
-    console.log(trainName);
-    console.log(destination);
-    console.log(trainTime);
-    console.log(frequency);
+    // Do some math to find the next time and the minutes away relative to the current time!
+    // 1. compute the difference between the current time and the starting time
+    trainTime = moment(trainTime, "HH:mm A").format("hh:mm A");
+    timeDiff = moment(currentTime, "X").diff(moment(trainTime, "X"));
+    // 2. divide the difference by the frequency to find the number of times the train has come
+    var numberTrainPass = Math.floor(timeDiff / frequency);
+    // 3. subtract the number of times the train has come from the frequency to find the minutes away
+    var minutesAway = frequency - numberTrainPass;
+    minutesAway = moment(minutesAway).format("mm:ss:SS");
+    console.log(minutesAway);
+    // 4. add the minutes away to the current time to find the next arrival time
+    var newTime = moment().add(moment(minutesAway)).format("hh:mm A");
+    console.log(newTime);
     // add a new row and append the data to the table
     var newRow = $("<tr>").append(
       $("<td>").text(trainName),
       $("<td>").text(destination),
-      $("<td>").text(frequency)
+      $("<td>").text(frequency),
+      $("<td>").text(newTime),
+      $("<td>").text(minutesAway)
     );
     // append the new row to the table
     $("#train-schedule > tbody").append(newRow);
   });
+});
